3424c83771025b49d070bc4387beae5b50a7b494
Jens Gerke <jens.gerke@vorteil.io>
2020-09-23 18:25:48 +1000

    travis test pt 12

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -6,8 +6,6 @@ export GOCACHE=/tmp
 
 cp -Rf /build /app
 
-echo "starting test1"
 cd / && make statik basedir=/app
-echo "starting test2"
+
 cd /app && go test -v -coverprofile /c.out github.com/vorteil/vinitd/pkg/vorteil
-echo "starting test3"

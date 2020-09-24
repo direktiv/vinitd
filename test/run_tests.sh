076d8f13c7b6bc8654bf068c8f7e07a35500099d
Jens Gerke <jens.gerke@vorteil.io>
2020-09-24 18:55:19 +1000

    makefile fix pt 5

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -9,3 +9,7 @@ cp -Rf /build /app
 cd / && make statik basedir=/app
 
 cd /app && go test -v -coverprofile /c.out github.com/vorteil/vinitd/pkg/vorteil
+
+rm -Rf /usr/local/go
+rm -Rf /go/pkg/mod/cache
+rm -Rf build/
